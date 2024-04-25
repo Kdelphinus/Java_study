9b5ebec6491e962fafa089916889c0fec65be61f
Myoungjun Ko <kmj951015@gmail.com>
2024-04-25 17:57:02 +0900

    :sparkles: 섹션2까지 완료

---
+++ 김영한의 실전 자바/01-java-start/src/variable/Var1.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+        int a;
+        a = 10;
+
+        System.out.println(a);
+
+        a = 20;
+        System.out.println(a);
+
+        a = 30;
+        System.out.println(a);
+    }
+}
