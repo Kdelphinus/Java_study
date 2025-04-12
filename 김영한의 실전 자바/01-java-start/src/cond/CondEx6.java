8033261a9f08712b7530c1c86201d7780d520f18
Myoungjun Ko <kmj951015@gmail.com>
2025-04-12 17:43:34 +0900

    스프링 폴더 삭제

--- 김영한의 실전 자바/01-java-start/src/cond/CondEx6.java
+++
@@ -1,8 +0,0 @@
-package cond;
-
-public class CondEx6 {
-    public static void main(String[] args) {
-        int a = 10, b = 20;
-        System.out.println(a > b ? a : b);
-    }
-}
--- 김영한의 실전 자바/01-java-start/src/cond/CondEx7.java
+++
@@ -1,8 +0,0 @@
-package cond;
-
-public class CondEx7 {
-    public static void main(String[] args) {
-        int x = 3;
-        System.out.println(x % 2 == 0 ? "even" : "odd");
-    }
-}
